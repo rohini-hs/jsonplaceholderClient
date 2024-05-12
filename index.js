7f7409acb94378cf676865e0cf5f70f5bb2d44d7
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-12 22:06:29 +0530

    added a table

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ function fetchResponse(){
     .then(data => {
         //find the messagediv
     let messageDiv = document.getElementById("messageId");
-    messageDiv.innerText = data.email + data.username + data.name ; 
+    //messageDiv.innerText =  data.email + data.username + data.name ; //
+
+
+    let table1 = document.getElementById("userTable");
+     fetch("http://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => {
+        for(let i=0; i<data.length; i++) {
+
+           let row = table1.insertRow(0);
+           let cell1 = row.insertCell(0);
+           let cell2 = row.insertCell(1);
+           cell1.innerHTML = data[i].name;
+           cell2.innerHTML = data[i].email;
+        }
+     });
 
      });
 
